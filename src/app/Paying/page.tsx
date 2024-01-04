3b3528a59d53b6801739a0fd7f14f107a5cb91ca
KCodeGuy <106859158+KCodeGuy@users.noreply.github.com>
2024-01-04 11:12:15 +0700

    upload base source

---
+++ src/app/Paying/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./Paying.module.scss";
+const Paying = () => {
+  return <div>{/* <h1 className={styles["red"]}>Paying</h1> */}</div>;
+};
+
+export default Paying;
