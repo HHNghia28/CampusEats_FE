cfb3207172d6d55984a891df1377a05df2d1649c
QuyenNNM0723 <144427889+quyen0723@users.noreply.github.com>
2024-01-06 11:02:23 +0700

    mm

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -7,62 +7,89 @@ import ButtonBase from '@/components/Buttons/Button';
 import ModalBase from '@/components/Modal/Modal';
 import ModalForm from '@/components/ModalForm/ModalForm';
 import { toast } from 'react-toastify';
+import classNames from 'classnames/bind';
+import styles from './Cart.module.scss';
+const cx = classNames.bind(styles);
 /*
   Page: Cart
   Author: QuyenNNM
 */
 interface CartItemProps {
-    updateTotal: (quantity: number) => void;
+  updateTotal: (quantity: number) => void;
 }
 
 const CartItem: React.FC<CartItemProps> = ({ updateTotal }) => {
-    const customStyle: React.CSSProperties = {
-        width: '140px',
-        height: '140px',
-        objectFit: 'cover' as 'cover', // Use string literal type directly
-    };
-    const [counter, setCounter] = useState(1);
+  const customStyle: React.CSSProperties = {
+    width: '140px',
+    height: '140px',
+    objectFit: 'cover' as 'cover'
+  };
+  const [counter, setCounter] = useState(1);
 
-    const handleIncrease = () => {
-        setCounter(prevState => prevState + 1)
-        updateTotal(1);
+  const handleIncrease = () => {
+    setCounter(prevState => prevState + 1);
+    updateTotal(1);
+  };
+
+  const handleDecrease = () => {
+    if (counter > 1) {
+      setCounter(counter - 1);
+      updateTotal(-1); // Giảm tổng số lượng khi giảm số lượng trong CartItem
     }
+  };
 
-    const handleDecrease = () => {
-        if (counter > 1) {
-            setCounter(counter - 1);
-            updateTotal(-1); // Giảm tổng số lượng khi giảm số lượng trong CartItem
-        }
-    }
+  return (
+    <div>
+      <Container>
+        <Row className={cx('d-flex', 'align-items-center')}>
+          <Col
+            md={3}
+            className={cx('d-flex', 'align-items-center', 'justify-content-center')}
+          >
+            <Image
+              src='https://pizzapoco.vn/wp-content/uploads/2021/05/pizzamoza.png'
+              style={customStyle}
+            />
+          </Col>
 
-    return (
-        <div>
-            <Container>
-                <Row className="d-flex align-items-center">
-                    <Col md={3} className="d-flex align-items-center justify-content-center">
+          <Col
+            md={6}
+            className={cx('d-flex', 'flex-column', 'align-items-start')}
+          >
+            <div className='py-2'>Name</div>
+            <div className='py-2'>Price</div>
+          </Col>
 
-                        <Image src="https://pizzapoco.vn/wp-content/uploads/2021/05/pizzamoza.png" style={customStyle} /></Col>
-
-                    <Col md={6} className="d-flex flex-column align-items-start">
-                        <div className='py-2'>Name</div>
-                        <div className='py-2'>Price</div>
-                    </Col>
-
-                    <Col md={3} className="d-flex align-items-center justify-content-center">
-                        <div className="d-flex align-items-center justify-center gap-2">
-                            <Button size='sm' onClick={handleDecrease} variant="outline-secondary">-</Button>
-                            <h5 className="mb-0">{counter}</h5>
-                            <Button size='sm' onClick={handleIncrease} variant="outline-secondary">+</Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+          <Col
+            md={3}
+            className={cx('d-flex', 'align-items-center', 'justify-content-center')}
+          >
+            <div
+              className={cx('d-flex', 'align-items-center', 'justify-center', 'gap-2')}
+            >
+              <Button
+                size='sm'
+                onClick={handleDecrease}
+                variant='outline-secondary'
+              >
+                -
+              </Button>
+              <h5 className={cx('mb-0')}>{counter}</h5>
+              <Button
+                size='sm'
+                onClick={handleIncrease}
+                variant='outline-secondary'
+              >
+                +
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
-const CartTotal = () => {
-
-};
+const CartTotal = () => {};
 
 export default CartItem;
