ca08c48a159a3fba23c0550884bdb01870d9cf38
Nghia Huynh <nghiahh.work@gmail.com>
2024-01-06 10:16:17 +0700

    Fix deploy

--- src/app/Paying/page.tsx
+++ src/app/Paying/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 import classNames from 'classnames/bind';
-import styles from './paying.module.scss';
+import styles from './Paying.module.scss';
 const cx = classNames.bind(styles);
 const Paying = () => {
   return (
